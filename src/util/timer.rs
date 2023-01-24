265aacf526af90ea9a7a76c832cc0cc7d7fe60f6
nahimr <nahim.rouidjali@gmail.com>
2023-01-24 22:11:26 +0100

    TimerCtxT -> TimerCtx

--- src/util/timer.rs
+++ src/util/timer.rs
@@ -1,24 +1,24 @@
 use libc::timeval;
 
 #[cfg(target_os = "windows")]
-struct TimerCtxT {
+struct TimerCtx {
     clock_start: u64,
 }
 
 #[cfg(not(target_os = "windows"))]
-struct TimerCtxT {
+struct TimerCtx {
     clock_start: timeval,
 }
 
 #[cfg(target_os = "windows")]
-fn util_init_hpctimer(mut timer: TimerCtxT) 
+fn util_init_hpctimer(mut timer: TimerCtx) 
 {
     use windows::Win32::System::Performance::QueryPerformanceCounter;
     QueryPerformanceCounter(&mut timer.clock_start);
 }
 
 #[cfg(not(target_os = "windows"))]
-fn util_init_hpctimer(mut timer: TimerCtxT) 
+fn util_init_hpctimer(mut timer: TimerCtx) 
 {
     use std::ptr;
     use libc::gettimeofday;
