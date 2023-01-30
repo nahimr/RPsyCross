2bcea99732cce509fa34f38badf3041dbb663323
Nahim Rouidjali <n.rouidjali@cyberdian.fr>
2023-01-30 16:24:51 +0100

    Test of signature and add test

--- tests/gte_test.rs
+++ tests/gte_test.rs
@@ -5,3 +5,9 @@ fn test_float_conversion() {
     let myFloat: f32 = 3.50;
     assert_eq!(3.50, from_half_float(to_half_float(myFloat)));
 }
+
+#[test]
+fn wrong_test_float_conversion() {
+    let myFloat: f32 = 3.0;
+    assert_ne!(3.50, from_half_float(to_half_float(myFloat)));
+}
